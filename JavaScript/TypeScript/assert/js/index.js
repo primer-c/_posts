4a4f5467886af36e3e195966812bdd8c9f44713b
web-oyster <yuanmin8888@163.com>
2020-10-26 10:29:55 +0800

    add index.js

---
+++ JavaScript/TypeScript/assert/js/index.js
@@ -0,0 +1,7 @@
+var Rose;
+(function (Rose) {
+    Rose[Rose["username"] = 0] = "username";
+    Rose[Rose["age"] = 1] = "age";
+    Rose[Rose["gender"] = 2] = "gender";
+})(Rose || (Rose = {}));
+console.log(Rose.username);
