d4ca1daa179555c5c5968c9c3b0231039f00d1c8
web-oyster <yuanmin8888@163.com>
2020-10-26 10:33:46 +0800

    delete index.js

--- JavaScript/TypeScript/assert/js/index.js
+++
@@ -1,7 +0,0 @@
-var Rose;
-(function (Rose) {
-    Rose[Rose["username"] = 0] = "username";
-    Rose[Rose["age"] = 1] = "age";
-    Rose[Rose["gender"] = 2] = "gender";
-})(Rose || (Rose = {}));
-console.log(Rose.username);
